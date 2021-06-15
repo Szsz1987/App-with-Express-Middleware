845eb20db2f96dad9ca8a0a4d7dd11c33b1ed18c
Szymon <sszulinski@gmail.com>
2021-06-15 16:55:24 +0200

    Task 27.3 - create the main page layout including navigation and footer

--- server.js
+++ server.js
@@ -3,39 +3,38 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => {
-  res.render('index', { layout: false });
-});
+app.get('/', (req,res) => {
+  res.render('index');
+})
 
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+app.get('/hello/:name', (req,res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about', {layout: 'dark'});
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { layout: false });
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', { layout: false });
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout: false });
+  res.render('history');
 });
 
-app.use((req, res) => {
+app.use((req,res) => {
   res.status(404).send('404 not found...');
-})
-
+});
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+  console.log(path.join(__dirname, 'Server is running on port: 8000'));
 });
